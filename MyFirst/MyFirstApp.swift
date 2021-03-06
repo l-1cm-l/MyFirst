6cce422b8fd1b2e505a3bd7213592d7b1f7658d2
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-03-06 09:28:56 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by administrator on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
